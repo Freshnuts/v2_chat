5f04b7b6148ecd5f89f6636881d6a0f71bf13fd1
Freshnuts <fr3shnuts@gmail.com>
2017-02-24 13:37:39 -0800

    Server file, under construction.

---
+++ v2_server.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define MAX_BUF 256
+#define PORTNUM 4441
+
+int parent_wrt();
+int child_rcv();
+pid_t pid;
+char greeting[] = "Welcome to Freshnuts' Encrypted Server\n";
+char disconnect[] = "DISCONNECT\n";
+int sockfd, newsockfd, clilen, n, mysock, conn_sock;
+char buffer[MAX_BUF];
+char message[200];
+struct sockaddr_in serv_addr, cli_addr;
+socklen_t socksize = sizeof(struct sockaddr_in);
+
+int main(int argc, char *argv) {
+
+  memset(&serv_addr, 0, sizeof(serv_addr)); //zero the structure
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(PORTNUM);
+
+
+// Intro
+  printf("\n==============================================================\n");
+  printf("v2 is a P2P Chat for Linux/Unix.\n");
+  printf("This program will allow you to chat directly\n"); 
+  printf("to client/server without a middle man.\n");
+  printf("\nCTRL + C to quit\n");
+  printf("==============================================================\n");
+// Create Socket
+  mysock = socket(AF_INET, SOCK_STREAM, 0);
+  if ( mysock < 0) {
+	perror("mysock Error: ");
+  }
+  else {
+	printf("Created Socket.\n");
+  }
+// Bind Socket
+  if (bind(mysock,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+	perror("bind Error: ");
+	exit(0);
+  }
+  else {
+	printf("Binded Socket.\n");
+  }
+
+  if (listen(mysock, 3) < 0) {
+	perror("Error: ");
+	exit(0);
+  }
+  printf("Listening for Connections.\n");
+
+// Accept Client
+  conn_sock = accept(mysock, (struct sock_addr *)&cli_addr, &socksize);  
+  if (conn_sock < 0) {
+	perror("Cannot Accept Client");
+  }
+
+// Greeting
+	printf("Incoming Connection from: %s\n", inet_ntoa(cli_addr.sin_addr));
+	write(conn_sock, greeting, sizeof(greeting));
+	recv(conn_sock, buffer, sizeof(buffer), 0);
+	printf("%s #> %s\n", inet_ntoa(cli_addr.sin_addr), buffer);
+
+// Chat Loop
+  pid = fork();
+
+  if (pid == 0 ) {
+	child_rcv();
+  } else if (pid > 0) {
+	parent_wrt();
+  } else {
+	printf("Failed to fork\n");
+  }
+
+  close(mysock);
+  return 0;
+}
+
+//Parent Process (Write to Socket Function)
+int parent_wrt() {
+  while (conn_sock) {
+	fgets(message, sizeof(message), stdin);
+    if (strcmp(message, disconnect) == 0) {
+//	  write(conn_sock,disconnected, sizeof(disconnected));
+      close(conn_sock);
+	  exit(0);
+    } else {
+	  write(conn_sock, message, sizeof(message));
+	  memset(message, 0, sizeof(message));
+	}
+  }
+  return 0;
+}
+// Child Process (Recover from Socket Function)
+int child_rcv() {
+  while (conn_sock) {
+	if (recv(conn_sock, buffer, sizeof(buffer), 0) <= 0) {
+	  perror("Disconnected by Client: ");
+	  close(conn_sock);
+	  exit(0);
+	} else {
+	  printf("%s #> %s", inet_ntoa(cli_addr.sin_addr), buffer);
+	  memset(buffer, 0, sizeof(buffer));
+	}
+  }
+  return 0;
+}
